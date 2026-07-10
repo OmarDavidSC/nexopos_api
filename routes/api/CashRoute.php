278559b78212cee8787796bdc3614a8320094f28
Omar SC <serquencoronadoomardavid@gmail.com>
2026-07-10 11:30:36 -0500

    caja

--- routes/api/CashRoute.php
+++ routes/api/CashRoute.php
@@ -1,37 +1,37 @@
 <?php
 
-$map->attach('categories.', '/category', function ($map) {
+$map->attach('cahses.', '/cash', function ($map) {
 
     $map->post('index', '', [
-        'Controller' => 'App\Controllers\CategoryController',
+        'Controller' => 'App\Controllers\CashController',
         'Action' => 'index'
     ]);
     $map->get('show', '/{id}/show', [
-        'Controller' => 'App\Controllers\CategoryController',
+        'Controller' => 'App\Controllers\CashController',
         'Action' => 'show'
     ]);
     $map->post('store', '/store', [
-        'Controller' => 'App\Controllers\CategoryController',
+        'Controller' => 'App\Controllers\CashController',
         'Action' => 'store'
     ]);
     $map->post('update', '/{id}/update', [
-        'Controller' => 'App\Controllers\CategoryController',
+        'Controller' => 'App\Controllers\CashController',
         'Action' => 'update'
     ]);
     $map->post('opensession', '/opensession', [
-        'Controller' => 'App\Controllers\CategoryController',
+        'Controller' => 'App\Controllers\CashController',
         'Action' => 'opensession'
     ]);
     $map->post('closesession', '/closesession', [
-        'Controller' => 'App\Controllers\CategoryController',
+        'Controller' => 'App\Controllers\CashController',
         'Action' => 'closesession'
     ]);
     $map->post('income', '/income', [
-        'Controller' => 'App\Controllers\CategoryController',
+        'Controller' => 'App\Controllers\CashController',
         'Action' => 'income'
     ]);
     $map->post('expense', '/expense', [
-        'Controller' => 'App\Controllers\CategoryController',
+        'Controller' => 'App\Controllers\CashController',
         'Action' => 'expense'
     ]);
 });
